64aa39e113d25bc96f0164840fc38f4aa8cd96d8
Thandolwethu Maneli <u24780635@up.ac.za>
2025-03-03 12:53:56 +0200

    Add project files.

---
+++ maneliStudyGroup/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace maneliStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult StudentList()
+        {
+            List<Models.Students> info = new List<Models.Students>();
+
+            info.Add(new Models.Students { StudentNumber = "u23864118", Name = "Muhammad", Surname = "Ismali", Email = "u23864118@tuks.co.za" });
+            info.Add(new Models.Students { StudentNumber = "u24780635", Name = "Thandolwethu", Surname = "Maneli", Email = "u24780635@tuks.co.za" });
+            info.Add(new Models.Students { StudentNumber = "u24575764", Name = "Megan", Surname = "Norval", Email = "u24575764@tuks.co.za" });
+            info.Add(new Models.Students { StudentNumber = "u24981088", Name = "Demarcus", Surname = "Le Roux", Email = "u249810888@tuks.co.za" });
+            info.Add(new Models.Students { StudentNumber = "u24653498", Name = "Lethokuhle", Surname = "Mabanga", Email = "uu24653498@tuks.co.za" });
+
+            return View(info);
+           
+        }
+    }
+}
---
+++ maneliStudyGroup/Models/students.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace maneliStudyGroup.Models
+{
+    public class Students
+    {
+
+        [Display(Name = "Student Number")] //Add as decorator
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "Name")] //Add as decorator
+        public string Name { get; set; }
+
+        [Display(Name = "Surname")] //Add as decorator
+        public string Surname { get; set; }
+
+        [Display(Name = "Email")] //Add as decorator
+        // [Required(ErrorMessage = "Name is mandatory")]=> Example of manditory decorator
+        public string Email { get; set; }
+    }
+}
