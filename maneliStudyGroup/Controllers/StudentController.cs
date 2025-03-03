90bd7bb8df3b77f6caab8674fe16dbfb67fc1684
Thandolwethu Maneli <u24780635@up.ac.za>
2025-03-03 12:57:38 +0200

    surname

--- maneliStudyGroup/Controllers/StudentController.cs
+++ maneliStudyGroup/Controllers/StudentController.cs
@@ -17,7 +17,7 @@ namespace maneliStudyGroup.Controllers
             info.Add(new Models.Students { StudentNumber = "u24780635", Name = "Thandolwethu", Surname = "Maneli", Email = "u24780635@tuks.co.za" });
             info.Add(new Models.Students { StudentNumber = "u24575764", Name = "Megan", Surname = "Norval", Email = "u24575764@tuks.co.za" });
             info.Add(new Models.Students { StudentNumber = "u24981088", Name = "Demarcus", Surname = "Le Roux", Email = "u249810888@tuks.co.za" });
-            info.Add(new Models.Students { StudentNumber = "u24653498", Name = "Lethokuhle", Surname = "Mabanga", Email = "uu24653498@tuks.co.za" });
+            info.Add(new Models.Students { StudentNumber = "u24653498", Name = "Lethokuhle", Surname = "Khoza", Email = "uu24653498@tuks.co.za" });
 
             return View(info);
            
